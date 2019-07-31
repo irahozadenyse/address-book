1574cb78d921d977c4a9f944a5729e48b94aff97
irahozadenyse <irahodeny1@gmail.com>
2019-07-31 12:40:33 +0200

    added resefield in js

--- js/scrpits.js
+++ js/scrpits.js
@@ -26,6 +26,14 @@ jane.addresses.push(home);
   return this.street + ", " + this.city + ", " + this.county;
 }
 
+
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-county").val("");
+}
  
 
 // user interface logic
